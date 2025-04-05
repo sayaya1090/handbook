7be4b3f2a82b74b0cd0ea1dffcb2689c35e7f0aa
sayaya <sayaya1090@hotmail.com>
2025-04-06 05:20:03 +0900

    쉘 UI 모듈 추가 및 관련 설정 업데이트
    [feat] 새로운 쉘 UI 모듈을 추가하고 `shell-ui` 관련 Gradle 설정, 워크플로우, HTML 템플릿 및 GWT 설정을 구성했습니다. 또한 주요 종속성과 플러그인의 버전을 업데이트했습니다.

---
+++ shell-ui/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    kotlin("jvm")
+    id("war")
+    id("dev.sayaya.gwt")
+    id("com.adarshr.test-logger")
+}
+dependencies {
+    implementation(libs.bundles.sayaya.web)
+    annotationProcessor(libs.lombok)
+    annotationProcessor(libs.dagger.compiler)
+    testImplementation(libs.bundles.test.web)
+    testAnnotationProcessor(libs.lombok)
+    testAnnotationProcessor(libs.dagger.compiler)
+}
+gwt {
+    gwtVersion = "2.12.2"
+    modules = listOf("dev.sayaya.handbook.Shell")
+    sourceLevel = "auto"
+    war = file("src/main/webapp")
+    devMode {
+        modules = listOf(
+            "dev.sayaya.handbook.Shell"
+        )
+        war = file("src/test/webapp")
+    }
+    generateJsInteropExports = true
+    compiler {
+        strict = true
+    }
+}
+tasks.named("war", War::class) {
+    archiveFileName.set("shell-ui.war")
+    duplicatesStrategy = DuplicatesStrategy.WARN
+}
---
+++ shell-ui/src/main/java/dev/sayaya/handbook/client/Application.java
@@ -0,0 +1,12 @@
+package dev.sayaya.handbook.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+import static org.jboss.elemento.Elements.body;
+
+public class Application implements EntryPoint {
+    @Override
+    public void onModuleLoad() {
+        body().add("Hello, World!!");
+    }
+}
