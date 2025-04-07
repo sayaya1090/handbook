bea54002b156ea2703d55798d70c742bc56ed3e8
sayaya <sayaya1090@hotmail.com>
2025-04-07 23:35:33 +0900

    추가: ProgressBuilder 클래스 및 관련 로직 업데이트
    [feat] ProgressBuilder 클래스를 추가하여 Progress 객체 생성을 단순화했습니다. getter 및 builder 패턴이 Progress 클래스에 적용되었으며, 이에 따라 기존 코드를 수정했습니다.

--- activity/src/main/java/dev/sayaya/handbook/client/domain/Progress.java
+++ activity/src/main/java/dev/sayaya/handbook/client/domain/Progress.java
@@ -4,15 +4,27 @@ import jsinterop.annotations.JsIgnore;
 import jsinterop.annotations.JsOverlay;
 import jsinterop.annotations.JsPackage;
 import jsinterop.annotations.JsType;
-import lombok.Setter;
+import lombok.Getter;
 import lombok.experimental.Accessors;
 
 @JsType(isNative=true, namespace= JsPackage.GLOBAL, name="Object")
-@Setter(onMethod_ = { @JsOverlay, @JsIgnore })
+@Getter(onMethod_ = { @JsOverlay, @JsIgnore })
 @Accessors(fluent = true)
 public final class Progress {
-    public boolean enabled;
-    public boolean intermediate;
-    public Double value;
-    public Double max;
+    private boolean enabled;
+    private boolean intermediate;
+    private Double value;
+    private Double max;
+
+    @JsOverlay @JsIgnore
+    static Progress of(boolean enabled, boolean intermediate, Double value, Double max) {
+        Progress progress = new Progress();
+        progress.enabled = enabled;
+        progress.intermediate = intermediate;
+        progress.value = value;
+        progress.max = max;
+        return progress;
+    }
+    @JsOverlay @JsIgnore
+    public static ProgressBuilder builder() { return new ProgressBuilder(); }
 }
--- shell-ui/src/main/java/dev/sayaya/handbook/client/interfaces/ProgressElement.java
+++ shell-ui/src/main/java/dev/sayaya/handbook/client/interfaces/ProgressElement.java
@@ -1,6 +1,7 @@
 package dev.sayaya.handbook.client.interfaces;
 
 import dev.sayaya.handbook.client.domain.Progress;
+import dev.sayaya.handbook.client.usecase.ClientWindow;
 import dev.sayaya.rx.Observable;
 import dev.sayaya.ui.elements.ProgressElementBuilder;
 import elemental2.dom.CSSProperties;
@@ -14,15 +15,16 @@ public class ProgressElement {
     @Delegate private final ProgressElementBuilder.LinearProgressElementBuilder _this = ProgressElementBuilder.progress().linear();
     @Inject ProgressElement(Observable<Progress> value) {
         value.subscribe(this::update);
+        ClientWindow.progress.next(Progress.builder().enabled(true).intermediate(true).build());
     }
     private void update(Progress value) {
-        if(!value.enabled) {
+        if(!value.enabled()) {
             _this.element().style.opacity = CSSProperties.OpacityUnionType.of("0");
         } else {
             _this.element().style.opacity = CSSProperties.OpacityUnionType.of("100");
-            _this.indeterminate(value.intermediate);
-            _this.element().max = value.max;
-            _this.value(value.value);
+            _this.indeterminate(value.intermediate());
+            _this.element().max = value.max();
+            _this.value(value.value());
         }
     }
 }
