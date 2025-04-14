040ebe43d4d21477055b78db4ec571e5cffe77cf
sayaya <sayaya1090@hotmail.com>
2025-04-15 01:12:08 +0900

    Add authentication module and RSA public key configuration
    [feat] authentication 모듈을 build script와 test 코드에 추가하였습니다. RSA public key를 생성 및 등록하여 JWT 인증을 지원하도록 구성했습니다.

--- persist/src/test/kotlin/dev/sayaya/handbook/ActuatorTest.kt
+++ persist/src/test/kotlin/dev/sayaya/handbook/ActuatorTest.kt
@@ -9,6 +9,9 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
 import org.springframework.test.web.reactive.server.WebTestClient
+import java.security.KeyPairGenerator
+import java.security.interfaces.RSAPublicKey
+import java.util.*
 
 @SpringBootTest(properties = [
     "management.endpoint.health.probes.enabled=true",
@@ -17,6 +20,7 @@ import org.springframework.test.web.reactive.server.WebTestClient
     "management.prometheus.metrics.export.enabled=true",
     "logging.level.io.r2dbc.postgresql.QUERY=DEBUG",
     "logging.level.io.r2dbc.postgresql.PARAM=DEBUG",
+    "spring.security.authentication.header=Authentication"
 ], webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
 internal class ActuatorTest(
@@ -56,10 +60,21 @@ internal class ActuatorTest(
     }
 }) {
     companion object {
+        private val PUBLIC_KEY: String
+        init {
+            val rsaKeyPair = KeyPairGenerator.getInstance("RSA").apply { initialize(2048) }.generateKeyPair()
+            val publicKey = (rsaKeyPair.public as RSAPublicKey)
+            PUBLIC_KEY = """
+                -----BEGIN PUBLIC KEY-----
+                ${Base64.getEncoder().encodeToString(publicKey.encoded)}
+                -----END PUBLIC KEY-----
+            """.trimIndent()
+        }
         @JvmStatic
         @DynamicPropertySource
         fun registerDynamicProperties(registry: DynamicPropertyRegistry) {
             Database().registerDynamicProperties(registry)
+            registry.add("spring.security.authentication.jwt.secret") { PUBLIC_KEY }
         }
     }
 }
