a333f5eae079425d40d6f29e6a3ab0dc935dbbb8
sayaya <sayaya1090@hotmail.com>
2025-04-15 03:13:40 +0900

    feat: 로그인 UI 모듈 추가
    [feat] GWT 기반의 로그인 UI 모듈을 생성하여 구조를 확장했습니다. 신규 Component 인터페이스와 Application 엔트리포인트를 포함하며, 빌드 및 리소스 관리 작업을 설정했습니다.

---
+++ login-ui/src/main/java/dev/sayaya/handbook/client/Application.java
@@ -0,0 +1,14 @@
+package dev.sayaya.handbook.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class Application implements EntryPoint {
+    private final Component components = DaggerComponent.create();
+    @Override
+    public void onModuleLoad() {
+        components.renderer().next(frame-> {
+            frame.append("Hello");
+           return true;
+        });
+    }
+}
