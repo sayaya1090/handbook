d8264027c4514dae823287564d0cce486307c28e
sayaya <sayaya1090@hotmail.com>
2025-04-20 08:25:05 +0900

    Log `CreateApplication` 로드 및 배포 경로 수정
    [feat] 모듈 로드 시 콘솔 로그 추가 및 배포 스크립트에 workspaceCreate 경로 처리 추가. 이는 디버깅과 배포 파일 관리 효율성을 개선합니다.

--- workspace-ui/src/main/java/dev/sayaya/handbook/client/interfaces/create/DialogElement.java
+++ workspace-ui/src/main/java/dev/sayaya/handbook/client/interfaces/create/DialogElement.java
@@ -1,6 +1,7 @@
 package dev.sayaya.handbook.client.interfaces.create;
 
 import elemental2.dom.HTMLDivElement;
+import elemental2.dom.HTMLLabelElement;
 import lombok.experimental.Delegate;
 import org.jboss.elemento.HTMLContainerBuilder;
 import org.jboss.elemento.IsElement;
@@ -9,11 +10,24 @@ import javax.inject.Inject;
 import javax.inject.Singleton;
 
 import static org.jboss.elemento.Elements.div;
+import static org.jboss.elemento.Elements.label;
 
 @Singleton
 public class DialogElement implements IsElement<HTMLDivElement> {
     @Delegate private final HTMLContainerBuilder<HTMLDivElement> div = div().css("dialog");
+    private final HTMLContainerBuilder<HTMLLabelElement> lblCreateWorkspace = label();
+    private final HTMLContainerBuilder<HTMLLabelElement> lblOr = label();
+    private final HTMLContainerBuilder<HTMLLabelElement> lblJoinWorkspace = label();
     @Inject DialogElement() {
-
+        div.add(lblCreateWorkspace).add(lblOr).add(lblJoinWorkspace);
+        //labels.subscribe(this::update);
+        update();
+    }
+    private void update() {
+        lblCreateWorkspace.element().innerHTML = "새로운 워크스페이스를 생성하세요.";
+        lblOr.element().innerHTML = "또는";
+        lblJoinWorkspace.element().innerHTML = "운영 중인 워크스페이스에 가입하세요. 워크스페이스 ID는 워크스페이스 관리자에게 문의하세요.";
+        //headline.element().innerHTML = findLabelOrDefault(label, menu.title()).toUpperCase();
+        //supportingText.element().innerHTML = findLabelOrDefault(label, menu.supportingText());
     }
 }
--- workspace-ui/src/test/java/dev/sayaya/handbook/client/create/Component.java
+++ workspace-ui/src/test/java/dev/sayaya/handbook/client/create/Component.java
@@ -1,11 +1,15 @@
 package dev.sayaya.handbook.client.create;
 
 import dev.sayaya.handbook.client.interfaces.create.ContentElement;
+import dev.sayaya.handbook.client.usecase.ClientSharedModule;
+import dev.sayaya.handbook.client.usecase.HostSharedModule;
+import dev.sayaya.rx.subject.BehaviorSubject;
 
 import javax.inject.Singleton;
 
 @Singleton
-@dagger.Component
+@dagger.Component(modules = { HostSharedModule.class, ClientSharedModule.class })
 public interface Component {
     ContentElement contentElement();
+    BehaviorSubject<String> uri();
 }
