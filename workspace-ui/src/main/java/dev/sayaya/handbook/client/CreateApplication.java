095a93053d4de2c0e4f3122febc8cbd63a90d2c7
sayaya <sayaya1090@hotmail.com>
2025-04-20 07:16:30 +0900

    Log `CreateApplication` 로드 및 배포 경로 수정
    [feat] 모듈 로드 시 콘솔 로그 추가 및 배포 스크립트에 workspaceCreate 경로 처리 추가. 이는 디버깅과 배포 파일 관리 효율성을 개선합니다.

--- workspace-ui/src/main/java/dev/sayaya/handbook/client/CreateApplication.java
+++ workspace-ui/src/main/java/dev/sayaya/handbook/client/CreateApplication.java
@@ -1,12 +1,14 @@
 package dev.sayaya.handbook.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import elemental2.dom.DomGlobal;
 
 public class CreateApplication implements EntryPoint {
     private final CreateComponent components = DaggerCreateComponent.create();
     @Override
     public void onModuleLoad() {
         components.renderer().next(frame-> {
+            DomGlobal.console.log("CreateApplication");
             frame.append(components.contentElement().element());
             return true;
         });
