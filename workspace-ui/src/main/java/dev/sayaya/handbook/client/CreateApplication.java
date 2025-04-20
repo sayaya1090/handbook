58e00c1518a24268dd62ec65d597058f2e65635f
sayaya <sayaya1090@hotmail.com>
2025-04-20 09:41:27 +0900

    워크스페이스 생성 및 가입 UI 구현
    [feat] 새로운 워크스페이스 생성 및 가입 기능을 위한 UI를 추가했습니다. DialogElement와 관련 컴포넌트들을 개선하고, LanguagePackManager 초기화를 추가했습니다. 테스트 HTML 파일도 새로 생성했습니다.

--- workspace-ui/src/main/java/dev/sayaya/handbook/client/CreateApplication.java
+++ workspace-ui/src/main/java/dev/sayaya/handbook/client/CreateApplication.java
@@ -1,14 +1,16 @@
 package dev.sayaya.handbook.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import elemental2.dom.DomGlobal;
+import elemental2.dom.HTMLLinkElement;
+
+import static org.jboss.elemento.Elements.htmlElement;
 
 public class CreateApplication implements EntryPoint {
     private final CreateComponent components = DaggerCreateComponent.create();
     @Override
     public void onModuleLoad() {
         components.renderer().next(frame-> {
-            DomGlobal.console.log("CreateApplication");
+            frame.append(htmlElement("link", HTMLLinkElement.class).attr("rel", "stylesheet").attr("href", "css/workspace.css").element());
             frame.append(components.contentElement().element());
             return true;
         });
