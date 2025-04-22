924a87e184ac0b45f6e9c797926f76f60a1c22a5
sayaya <sayaya1090@hotmail.com>
2025-04-23 04:42:07 +0900

    이동 및 R2dbcAuditorConfig 로직 개선
    [refactor] R2dbcAuditorConfig를 이동하고 보안 컨텍스트에서 인증 정보를 활용하도록 개선했습니다. 이를 통해 R2DBC 감사 정보를 보다 정확하게 처리할 수 있습니다. Dependencies에 r2dbc 추가했습니다.

---
+++ authentication/src/main/kotlin/dev/sayaya/handbook/interfaces/authentication/R2dbcAuditorConfig.kt
@@ -0,0 +1,25 @@
+package dev.sayaya.handbook.interfaces.authentication
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.domain.ReactiveAuditorAware
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.ReactiveSecurityContextHolder
+import org.springframework.security.core.context.SecurityContext
+import java.time.Duration
+import java.util.*
+
+@Configuration
+@ConditionalOnMissingBean(ReactiveAuditorAware::class)
+@EnableR2dbcAuditing
+class R2dbcAuditorConfig {
+    @Bean fun auditorProvider(): ReactiveAuditorAware<UUID> = ReactiveAuditorAware {
+        ReactiveSecurityContextHolder.getContext()
+            .timeout(Duration.ofSeconds(1))
+            .map { obj: SecurityContext -> obj.authentication }
+            .filter { obj: Authentication -> obj.isAuthenticated }
+            .map { obj: Authentication -> UUID.fromString(obj.principal as String) }
+    }
+}
