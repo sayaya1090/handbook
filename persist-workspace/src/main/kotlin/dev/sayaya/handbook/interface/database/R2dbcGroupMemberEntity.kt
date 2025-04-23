63869def5546ea9153baeff5b6f267231b07727c
sayaya <sayaya1090@hotmail.com>
2025-04-24 01:17:11 +0900

    Add admin group creation to workspace save process
    [feat] 워크스페이스 생성 시 Admin 그룹을 자동 생성하고 생성자를 배정하도록 수정했습니다. 이를 위해 R2DBC 리포지토리와 테스트 코드를 추가 및 수정했습니다.

--- persist-workspace/src/main/kotlin/dev/sayaya/handbook/interface/database/R2dbcGroupMemberEntity.kt
+++ persist-workspace/src/main/kotlin/dev/sayaya/handbook/interface/database/R2dbcGroupMemberEntity.kt
@@ -2,6 +2,7 @@ package dev.sayaya.handbook.`interface`.database
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.annotation.Transient
+import org.springframework.data.relational.core.mapping.Column
 import org.springframework.data.relational.core.mapping.Table
 import java.io.Serializable
 import java.util.*
@@ -9,7 +10,7 @@ import java.util.*
 @Table("group_member")
 data class R2dbcGroupMemberEntity (
     val workspace: UUID,
-    val group: String,
+    @Column("\"group\"") val group: String,
     val member: UUID
 ) {
     @Transient @Id val pk: R2dbcGroupMemberId = R2dbcGroupMemberId(workspace, group, member)
--- persist-workspace/src/main/kotlin/dev/sayaya/handbook/usecase/WorkspaceService.kt
+++ persist-workspace/src/main/kotlin/dev/sayaya/handbook/usecase/WorkspaceService.kt
@@ -2,6 +2,7 @@ package dev.sayaya.handbook.usecase
 
 import dev.sayaya.handbook.domain.Workspace
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import reactor.core.publisher.Mono
 import java.security.Principal
 
@@ -12,6 +13,7 @@ class WorkspaceService(
     private val eventHandler: ExternalServiceHandler,
 ) {
     // 새로운 워크스페이스 생성 시, 워크스페이스를 생성하고 Workspace Admin 그룹을 만들어 생성자를 배정한다.
+    @Transactional
     fun save(principal: Principal, workspaceBuilder: WorkspaceBuilder): Mono<Workspace> = workspaceBuilder.build()
         .let(workspaceRepo::save)
         .delayUntil { groupRepo.createAndAssign(
