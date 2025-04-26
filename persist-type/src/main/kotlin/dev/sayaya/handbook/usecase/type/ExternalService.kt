e26d3161f06034d001aee309fb5f6aa275009149
sayaya <sayaya1090@hotmail.com>
2025-04-27 04:23:05 +0900

    폴더명을 persist에서 persist-type으로 변경
    [refactor] 코드와 리소스의 폴더 구조 일관성 확보를 위해 persist 폴더를 persist-type으로 리팩터링하였습니다. 이에 따라 모든 관련 경로와 설정을 업데이트했습니다.

--- persist-type/src/main/kotlin/dev/sayaya/handbook/usecase/type/ExternalService.kt
+++ persist-type/src/main/kotlin/dev/sayaya/handbook/usecase/type/ExternalService.kt
@@ -2,7 +2,7 @@ package dev.sayaya.handbook.usecase.type
 
 import dev.sayaya.handbook.domain.Type
 import reactor.core.publisher.Mono
-import java.util.UUID
+import java.util.*
 
 interface ExternalService {
     fun publish(workspace: UUID, type: Type): Mono<Void>
