76d80ca8da0531df64bf8fd0a9b24807a0811227
sayaya <sayaya1090@hotmail.com>
2025-04-30 18:34:30 +0900

    리팩토링: R2dbcTypeLayout 매핑 방식 변경 및 불필요 코드 제거
    [refactor] 기존 R2dbcTypeLayoutEntity 매핑 방식을 제거하고 직접 Row 매핑 방식으로 변경했습니다. 동작 일관성을 유지하며 코드 간소화를 달성했습니다.

--- search-type/src/main/kotlin/dev/sayaya/handbook/interface/database/R2dbcTypeLayoutRepository.kt
+++ search-type/src/main/kotlin/dev/sayaya/handbook/interface/database/R2dbcTypeLayoutRepository.kt
@@ -14,18 +14,31 @@ class R2dbcTypeLayoutRepository(private val template: R2dbcEntityTemplate): Layo
     override fun findByBaseTime(workspace: UUID, baseTime: Instant): Flux<TypeWithLayout> = template.databaseClient.sql(sql)
             .bind("baseTime", baseTime) // 바인딩된 baseTime 값
             .bind("workspace", workspace) // 바인딩된 workspace ID 값
-            .mapValue(R2dbcTypeLayoutEntity::class.java) // R2dbcTypeLayoutEntity로 매핑
-            .all()
-            .map { it.toDomain() }
+            .map { row ->
+                TypeWithLayout(
+                    type = Type(
+                        id = row.get("name", String::class.java)!!,
+                        version = row.get("version", String::class.java)!!,
+                        effectDateTime = row.get("effective_at", Instant::class.java)!!,
+                        expireDateTime = row.get("expire_at", Instant::class.java)!!,
+                        description = row.get("description", String::class.java) ?: "",
+                        primitive = row.get("primitive", Boolean::class.java) ?: false,
+                        attributes = emptyList(),
+                        parent = row.get("parent", String::class.java),
+                    ), x = row.get("x", Short::class.java)!!.unsigned(),
+                    y = row.get("y", Short::class.java)!!.unsigned(),
+                    width = row.get("width", Short::class.java)!!.unsigned(),
+                    height = row.get("height", Short::class.java)!!.unsigned()
+                )
+            }.all()
 
     companion object {
         private val sql = """
-            SELECT t.workspace, t.id, t.name, t.version, t.parent, 
-            t.effective_at, t.expire_at, t.description, t.primitive, t.last,
-            t.created_at, t.created_by,
-            l.layout, l.x, l.y, l.width, l.height
+            SELECT t.name, t.version, t.parent, 
+            t.effective_at, t.expire_at, t.description, t.primitive, 
+            l.x, l.y, l.width, l.height
             FROM type t 
-            INNER JOIN layout_type l
+            INNER JOIN layout_type l 
             ON t.workspace = l.workspace AND t.name = l.type AND t.version = l.version AND t.last=true
             INNER JOIN layout p
             ON l.layout = p.id
@@ -33,21 +46,6 @@ class R2dbcTypeLayoutRepository(private val template: R2dbcEntityTemplate): Layo
             AND p.expire_at > :baseTime
             AND t.workspace = :workspace
         """.trimIndent()
-        fun R2dbcTypeLayoutEntity.toDomain(): TypeWithLayout = TypeWithLayout (
-            type = Type(
-                id = this.name,
-                version = this.version,
-                effectDateTime = this.effectDateTime,
-                expireDateTime = this.expireDateTime,
-                description = this.description,
-                primitive = this.primitive,
-                attributes = emptyList(),
-                parent = this.parent,
-            ), x = this.x.unsigned(),
-            y = this.y.unsigned(),
-            width = this.width.unsigned(),
-            height = this.height.unsigned()
-        )
         fun Short.unsigned(): UShort = (this + 32768).toUShort()
     }
 }
