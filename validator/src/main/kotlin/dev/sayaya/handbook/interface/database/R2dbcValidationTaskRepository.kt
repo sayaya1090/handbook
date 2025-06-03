7f9d79750c7de81211700c174f84fb112bd68529
sayaya <sayaya1090@hotmail.com>
2025-06-04 08:53:12 +0900

    ..
    Signed-off-by: sayaya <sayaya1090@hotmail.com>

--- validator/src/main/kotlin/dev/sayaya/handbook/interface/database/R2dbcValidationTaskRepository.kt
+++ validator/src/main/kotlin/dev/sayaya/handbook/interface/database/R2dbcValidationTaskRepository.kt
@@ -7,14 +7,12 @@ import dev.sayaya.handbook.usecase.ValidationTaskRepository
 import io.r2dbc.postgresql.codec.Json
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.data.r2dbc.config.EnableR2dbcAuditing
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Mono
 import java.util.UUID
 
 @Repository
-@EnableR2dbcAuditing
 class R2dbcValidationTaskRepository (
     private val template: R2dbcEntityTemplate,
     private val objectMapper: ObjectMapper
