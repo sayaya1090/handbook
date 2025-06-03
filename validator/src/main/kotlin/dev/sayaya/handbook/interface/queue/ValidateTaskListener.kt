01036191c65b9459f95d018e5dcf2b3186309193
sayaya <sayaya1090@hotmail.com>
2025-06-04 08:44:11 +0900

    ..
    Signed-off-by: sayaya <sayaya1090@hotmail.com>

--- validator/src/main/kotlin/dev/sayaya/handbook/interface/queue/ValidateTaskListener.kt
+++ validator/src/main/kotlin/dev/sayaya/handbook/interface/queue/ValidateTaskListener.kt
@@ -1,25 +1,18 @@
 package dev.sayaya.handbook.`interface`.queue
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import dev.sayaya.handbook.domain.event.DocumentEvent
 import dev.sayaya.handbook.domain.event.Event
-import dev.sayaya.handbook.usecase.ValidatorService
+import dev.sayaya.handbook.`interface`.event.EventProcessor
 import org.springframework.stereotype.Component
 import java.util.function.Consumer
 
 @Component("request-listener")
 class ValidateTaskListener(
-    private val validator: ValidatorService,
-    private val om: ObjectMapper
+    private val om: ObjectMapper,
+    private val processor: EventProcessor
 ): Consumer<String> {
-    override fun accept(event: String) = synchronized(this) {
-        try {
-            val event = om.readValue(event, Event::class.java)
-            when {
-                event is DocumentEvent && event.type!= Event.EventType.DELETE_DOCUMENT  -> validator.validate(event)
-            }
-        } catch (e: Exception) {
-            throw IllegalStateException("Failed to process event: $event", e)
-        }
+    override fun accept(event: String): Unit = synchronized(this) {
+        val event = om.readValue(event, Event::class.java)
+        processor.processEvent(event).subscribe()
     }
 }
