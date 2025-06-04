22331ca10f737448fa32e94e2cb14e6b5c3352d8
sayaya <sayaya1090@hotmail.com>
2025-06-04 09:23:44 +0900

    ..
    Signed-off-by: sayaya <sayaya1090@hotmail.com>

--- validator/src/main/kotlin/dev/sayaya/handbook/interface/database/R2dbcValidationTaskEntity.kt
+++ validator/src/main/kotlin/dev/sayaya/handbook/interface/database/R2dbcValidationTaskEntity.kt
@@ -1,7 +1,6 @@
 package dev.sayaya.handbook.`interface`.database
 
 import io.r2dbc.postgresql.codec.Json
-import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.Id
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.relational.core.mapping.Column
@@ -18,7 +17,7 @@ data class R2dbcValidationTaskEntity (
     var results: Json? = null
     var status: String = "NEW"
     var priority: Int = 0
-    @CreatedDate @Column("created_at") lateinit var createDateTime: Instant
+    @Column("created_at") val createDateTime: Instant = Instant.now()
     @Column("started_at") var startDateTime: Instant? = null
     @LastModifiedDate @Column("updated_at") lateinit var updateDateTime: Instant
     var retryCount: Int = 0
