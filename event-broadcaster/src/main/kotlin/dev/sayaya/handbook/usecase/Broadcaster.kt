24be1721bbed834167731930fc676b5340d6b475
sayaya <sayaya1090@hotmail.com>
2025-06-10 04:06:13 +0900

    feat: Workspace별 메시지 필터링 기능 추가
    - 워크스페이스별로 메시지를 구독할 수 있도록 API 수정
    - `@PathVariable`을 통해 워크스페이스 ID를 전달받아 구독 처리
    - `Broadcaster`에 워크스페이스별 이벤트 관리 로직 추가
    - 구독자가 필요한 이벤트만 수신하여 효율성 향상

--- event-broadcaster/src/main/kotlin/dev/sayaya/handbook/usecase/Broadcaster.kt
+++ event-broadcaster/src/main/kotlin/dev/sayaya/handbook/usecase/Broadcaster.kt
@@ -1,5 +1,6 @@
 package dev.sayaya.handbook.usecase
 
+import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
 import dev.sayaya.handbook.domain.event.Event
 import org.slf4j.Logger
@@ -7,15 +8,28 @@ import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Sinks
-import java.time.Duration
+import java.util.UUID
+import java.util.concurrent.ConcurrentHashMap
 
 @Service
 class Broadcaster(private val om: ObjectMapper) {
     private val logger: Logger = LoggerFactory.getLogger(javaClass)
-    private val sink: Sinks.Many<Event<*, *>> = Sinks.many().replay().limit(Duration.ofMillis(100))
+    private val sink: Sinks.Many<Event<*, *>> = Sinks.many().replay().limit(0)
+    private val workspaceSinks: ConcurrentHashMap<UUID, Sinks.Many<Event<*, *>>> = ConcurrentHashMap()
+    init {
+        sink.asFlux()
+            .doOnNext { event ->
+                workspaceSinks.computeIfAbsent(event.workspace) {
+                    Sinks.many().replay().limit(0)
+                }.tryEmitNext(event)
+            }.subscribe()
+    }
+
     fun broadcast(event: String) {
-        val emit = sink.tryEmitNext(om.readValue(event, Event::class.java))
+        val emit = sink.tryEmitNext(om.readValue(event, object : TypeReference<Event<*, *>>() {}))
         logger.info(emit.toString())
     }
-    fun listen(): Flux<Event<*, *>> = sink.asFlux()
+    fun listen(workspace: UUID): Flux<Event<*, *>> = workspaceSinks.computeIfAbsent(workspace) {
+        Sinks.many().replay().limit(0)
+    }.asFlux()
 }
